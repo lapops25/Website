a90213761332b3a437fa2c192eb8b625eb45b9ed
Lapops <augustineokpala2@gmail.com>
2025-07-02 05:17:56 +0100

    First commit

---
+++ movies/movies/src/main/java/dev/Augustine/movies/MovieController.java
@@ -0,0 +1,33 @@
+package dev.Augustine.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
+public class MovieController {
+
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.SingleMovie(imdbId), HttpStatus.OK);
+    }
+}
---
+++ movies/movies/src/main/java/dev/Augustine/movies/MoviesApplication.java
@@ -0,0 +1,17 @@
+package dev.Augustine.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+}
